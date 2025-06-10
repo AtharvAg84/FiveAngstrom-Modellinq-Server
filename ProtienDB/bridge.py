55b951c046a1fad9d890ea35b1a3c262d1630123
AtharvAg84 <atharv8484@gmail.com>
2025-06-10 11:59:01 +0530

    Update CORS configuration to allow additional origin for development

--- ProtienDB/bridge.py
+++ ProtienDB/bridge.py
@@ -20,7 +20,7 @@ app = FastAPI(title="PDB Search Bridge")
 # Enable CORS for React app
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:4173", "http://localhost:5173","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
